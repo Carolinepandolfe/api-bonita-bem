b1591f326ee4d144932d68768c4075c4483f1f87
carolinepandolfe <carol@MacBook-Pro-de-Caroline.local>
2023-01-28 14:45:23 -0300

    Connected mongo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const port = 3001;
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-app.listen(port,() => {
-  console.log(`🥑 Servidor bonito bem:  http://localhost:${port}`);
-});
+    app.listen(port,() => {
+      console.log(`🥑 Servidor bonito bem:  http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('erro'));
+
